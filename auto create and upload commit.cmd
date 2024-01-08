85712bae400b68eae5e3715d56fa5fbf74f857b3
Gogik <gog.ortey@yandex.ru>
2024-01-08 21:28:11 +1000

    9

---
+++ auto create and upload commit.cmd
@@ -0,0 +1,14 @@
+@echo off
+chcp 65001 >nul
+set /p commitMessage="Описание коммита: "
+if "%commitMessage%"=="" (
+    for /f "tokens=2 delims==" %%I in ('wmic os get localdatetime /format:list') do set datetime=%%I
+    set datetime=%datetime:~6,2%.%datetime:~4,2%.%datetime:~0,4% - %datetime:~8,2%:%datetime:~10,2% - no description
+    set commitMessage="%datetime%"
+)
+git add .
+git commit -m %commitMessage%
+git push origin main
+echo Коммит успешно создан и отправлен!
+timeout /t 2 >nul
+exit
